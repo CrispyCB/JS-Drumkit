54ac6ee1f6c52eb6819ae961a394c7ca04546468
Christian Bahnweg <cebahnweg@gmail.com>
2018-06-07 21:42:30 -0400

    fixes audio.play and adds audio.currentTime

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 window.addEventListener('keydown',function(e){
-const audio = document.querySelector(`audio[data-key="{e.keyCode}"]`);
+const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 audio.play();
+audio.currentTime = 0;
 if(!audio) return;
 });
